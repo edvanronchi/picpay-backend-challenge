9e437a9885c4a70429e8f416e64c33fe7f6e04e6
Edvan Ronchi <edvan.ronchi@betha.com.br>
2023-09-08 19:31:51 -0300

    #:FEAT: Create entities

---
+++ src/main/java/com/picpay/domain/transaction/Transaction.java
@@ -0,0 +1,37 @@
+package com.picpay.domain.transaction;
+
+import com.picpay.domain.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="TRANSACTIONS")
+@SequenceGenerator(name = "SEQ_TRANSACTIONS", sequenceName = "SEQ_TRANSACTIONS", allocationSize = 1)
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_TRANSACTIONS")
+    private Long id;
+
+    @Column(name = "VALUE", nullable = false)
+    private BigDecimal value;
+
+    @Column(name = "TIMESTAMP", nullable = false)
+    private LocalDateTime timestamp;
+
+    @ManyToOne
+    @JoinColumn(name = "PAYER_ID", nullable = false)
+    private User payer;
+
+    @ManyToOne
+    @JoinColumn(name = "PAYEE_ID", nullable = false)
+    private User payee;
+}
---
+++ src/main/java/com/picpay/domain/user/User.java
@@ -0,0 +1,44 @@
+package com.picpay.domain.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="USERS")
+@SequenceGenerator(name = "SEQ_USERS", sequenceName = "SEQ_USERS", allocationSize = 1)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_USERS")
+    private Long id;
+
+    @Column(name = "FIRST_NAME", nullable = false)
+    private String firstName;
+
+    @Column(name = "LAST_NAME", nullable = false)
+    private String lastName;
+
+    @Column(name = "PASSWORD", nullable = false)
+    private String password;
+
+    @Column(name = "BALANCE")
+    private BigDecimal balance;
+
+    @Column(name = "DOCUMENT", unique = true, nullable = false)
+    private String document;
+
+    @Column(name = "EMAIL", unique = true, nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "TYPE", nullable = false)
+    private TypeUser typeUser;
+}
